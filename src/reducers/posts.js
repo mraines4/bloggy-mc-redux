b3b5a1a546713923f43fedfaffc3354432a59351
Matt Raines <mraines4@gmail.com>
2019-05-09 09:48:23 -0400

    adds post reducer and redux

---
+++ src/reducers/posts.js
@@ -0,0 +1,13 @@
+import { ACTION_CREATE_POST } from "../actions"; 
+
+// a reducer is a function that accepts the current state 
+// and an action and then calculates/returns the next new version of state
+export function posts(state={}, action={type=''}) {
+    switch (action.type) {
+        case ACTION_CREATE_POST:
+        console.log('looks like you want to create a post - clippy');
+            break; 
+        default:
+            return state
+    }
+}
