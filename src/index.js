c7d8403d831a246ec72598747a074a1a4d16e6c4
Matt Raines <mraines4@gmail.com>
2019-05-09 11:21:33 -0400

    autosaves state to local storage

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import store from './store';
 window.store = store
 
+const frequency = 2000;
+const ls_key = 'bloggy-mc-redux'
+let saveInterval = setInterval(() => {
+    const state = store.getState()
+    localStorage.setItem(ls_key, JSON.stringify(state))
+},frequency)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
